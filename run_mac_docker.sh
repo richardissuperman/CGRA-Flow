498c535b0a5ee17415655cdf222fef78e403c73b
zhong qing <>
2025-03-01 11:09:23 -0800

    change my own docker container

--- run_mac_docker.sh
+++ run_mac_docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CONTAINER=cgra/cgra-flow:20241028
+CONTAINER=ssh_server_with_debugpy:latest
 NIC=en0
 
 # Grab the ip address of this box
 IPADDR=$(ifconfig $NIC | grep "inet " | awk '{print $2}')
-DISP_NUM=$(jot -r 1 100 200)  # random display number between 100 and 200
+DISP_NUM=0
 PORT_NUM=$((6000 + DISP_NUM)) # so multiple instances of the container won't interfer with eachother
 socat TCP-LISTEN:${PORT_NUM},reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" 2>&1 > /dev/null &
 
@@ -21,4 +21,6 @@ docker run \
     -v $XAUTH:$XAUTH:rw \
     -e DISPLAY=$IPADDR:$DISP_NUM \
     -e XAUTHORITY=$XAUTH \
+    -p 5678:5678 \
+    -p 2022:22 \
     $CONTAINER
